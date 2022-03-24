736ec3a2c7abc51611f2fe2ea72efe8194d621c3
Darrian Bagley <dbagle14@gmail.com>
2022-03-24 17:04:31 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
+// O(n^2)
 
 /* 
   Add your pseudocode here
 */
+/**  
+ * 1. Add together a unique combination of numbers
+ * 2. Check if the sum is equal to the target
+ * 3. If it is, return true
+ * 4. If it is not, continue to the next combination
+ * 5. If there are no more combinations, return false
+*/
 
 /*
   Add written explanation of your solution here
+*/
+/** 
+ * This function is O(n^2) because it loops through the array
+ * checking every combination of 2 elements until a matching sum is found.
+ * If no valid combination is found, it returns false.
 */
 
 // You can run `node index.js` to view these console logs
